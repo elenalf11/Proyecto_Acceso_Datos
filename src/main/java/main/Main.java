41cc2142b9701dc2daacac108d75cbe3d9aa291a
mayrabpi <145108717+mayrabpi@users.noreply.github.com>
2025-01-21 15:40:14 +0100

    commit2

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,10 @@
 package main;
 
 public class Main {
-    public static void main (String []args){
+    public static void main (String []args) {
         System.out.println("Hola mundo");
+        System.out.println("Hola");
+
     }
+
 }
