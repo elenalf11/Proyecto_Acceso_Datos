e381ea9a3cb3c99eca8dad81d46a336e5fe23300
elenalf11 <elena.lopezfelix@colegiolitterator.com>
2025-01-29 15:24:56 +0100

    Engine subido

--- src/main/java/main/Ventas.java
+++ src/main/java/main/Ventas.java
@@ -1,4 +1,8 @@
 package main;
 
 public class Ventas {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/proyecto?serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASS = "110805";
 }
