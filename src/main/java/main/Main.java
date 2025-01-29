318f67801beda329e56780f0ff4512fc660930aa
elenalf11 <elena.lopezfelix@colegiolitterator.com>
2025-01-29 16:59:02 +0100

    Todo subido

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,6 +1,16 @@
 package main;
 
+/**
+ * Clase Main
+ * En esta clase iniciamos el programa
+ */
 public class Main {
+
+    /**
+     * Metodo main
+     * Inicializa el programa
+     * @param args
+     */
     public static void main (String []args) {
         Engine eng = new Engine();
         eng.start();
--- src/main/java/main/Productos.java
+++ src/main/java/main/Productos.java
@@ -1,7 +1,14 @@
 package main;
 
+/**
+ * Clase Productos
+ * En esta clase nos encontraremos todos los metodos relacionados con la tabla Productos
+ */
 public class Productos {
 
+    /**
+     * Atributos
+     */
     private static final String URL = "jdbc:mysql://localhost:3306/proyecto?serverTimezone=UTC";
     private static final String USER = "root";
     private static final String PASS = "110805";
