984a0dca739892b0a5792c7ce27a0989bf0726ee
elenalf11 <elena.lopezfelix@colegiolitterator.com>
2025-01-29 19:06:25 +0100

    Menus creados de las clases Ventas, Empleados y Productos

--- src/main/java/main/Productos.java
+++ src/main/java/main/Productos.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.Scanner;
+
 /**
  * Clase Productos
  * En esta clase nos encontraremos todos los metodos relacionados con la tabla Productos
@@ -12,5 +14,33 @@ public class Productos {
     private static final String URL = "jdbc:mysql://localhost:3306/proyecto?serverTimezone=UTC";
     private static final String USER = "root";
     private static final String PASS = "110805";
+    private Scanner sc = new Scanner(System.in);
+
+    public void menu(){
+        int opcion;
+        do{
+            System.out.println("¿Qué quieres hacer?\n1. Añadir producto\n2. Actualizar datos\n3. Eliminar producto\n4. Ver todos los productos\n0. Salir");
+            opcion = sc.nextInt();
+            switch (opcion){
+                case 1:
+                    System.out.println("Añadir producto");
+                    break;
+                case 2:
+                    System.out.println("Actualizar datos");
+                    break;
+                case 3:
+                    System.out.println("Eliminar producto");
+                    break;
+                case 4:
+                    System.out.println("Ver todos los productos");
+                    break;
+                case 0:
+                    System.out.println("Saliendo ...");
+                    break;
+                default:
+                    System.out.println("Opción inválida, vuelve a seleccionar");
+            }
+        } while(opcion != 0 );
+    }
     
 }
--- src/main/java/main/Ventas.java
+++ src/main/java/main/Ventas.java
@@ -1,8 +1,38 @@
 package main;
 
+import java.util.Scanner;
+
 public class Ventas {
 
     private static final String URL = "jdbc:mysql://localhost:3306/proyecto?serverTimezone=UTC";
     private static final String USER = "root";
     private static final String PASS = "110805";
+    private Scanner sc = new Scanner(System.in);
+
+    public void menu(){
+        int opcion;
+        do{
+            System.out.println("¿Qué quieres hacer?\n1. Actualizar datos\n2. Eliminar venta\n3. Ver todas las ventas\n4. Ver información sobre el cliente\n0. Salir");
+            opcion = sc.nextInt();
+            switch (opcion){
+                case 1:
+                    System.out.println("Actualizar datos");
+                    break;
+                case 2:
+                    System.out.println("Eliminar venta");
+                    break;
+                case 3:
+                    System.out.println("Ver todas las ventas");
+                    break;
+                case 4:
+                    System.out.println("Ver información sobre el cliente ");
+                    break;
+                case 0:
+                    System.out.println("Saliendo ...");
+                    break;
+                default:
+                    System.out.println("Opción inválida, vuelve a seleccionar");
+            }
+        } while(opcion != 0 );
+    }
 }
